a4c86d01ac11e4f90d284bc2a09b865b44bfbf0d
andromeda <g.simon.levett@gmail.com>
2017-05-18 16:03:17 +1000

    cool

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
 
-  constructor() { }
+  tiles = [
+    {text:'1', cols: 2,rows:1 ,color:'lightblue'},
+    {text:'2', cols: 1,rows:2 ,color:'lightgreen'},
+    {text:'3', cols: 3,rows:1 ,color:'lightpink'},
+    {text:'4', cols: 1,rows:1 ,color:'red'},
+    {text:'5', cols: 2,rows:1 ,color:'blue'}
 
-  ngOnInit() {
-  }
+  ];
 
 }
