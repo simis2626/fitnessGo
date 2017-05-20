236278b78b05f387614703e85321fd93fcff2805
Andromeda <g.simon.levett@gmail.com>
2017-05-20 19:45:32 +1000

    working branding

--- src/app/branding/branding.component.ts
+++ src/app/branding/branding.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BrandingComponent implements OnInit {
 
   constructor() { }
-
+  brandCaption:string = "Tracking your fitness is easy";
   ngOnInit() {
   }
 
