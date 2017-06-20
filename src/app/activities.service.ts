016c855fb6e9699e4d9b867f19f1c086d01547f7
Andromeda <g.simon.levett@gmail.com>
2017-06-20 20:43:20 +1000

    material theme working

--- src/app/activities.service.ts
+++ src/app/activities.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@angular/core';
-import {Activity} from './Objects/Activity';
+import {Injectable} from "@angular/core";
+import {Activity} from "./Objects/Activity";
+import {Headers, Http, RequestOptions} from "@angular/http";
 
 @Injectable()
 export class ActivitiesService {
 
   private activities:Activity[];
-  constructor() {
+
+
+  headers: Headers;
+  options: RequestOptions;
+
+  constructor(private http: Http) {
     this.activities = [];
-    this.activities.push(new Activity(1234,'treadmill',true));
-    this.activities.push(new Activity(1235,'bike',true));
-    this.activities.push(new Activity(1236,'eleptical',true));
-    this.activities.push(new Activity(1237,'rowing',true));
-    this.activities.push(new Activity(1238,'bilateral rope pulldown',false));
-    this.activities.push(new Activity(1239,'pushups',false));
-    this.activities.push(new Activity(1249,'bird pees',false));
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.options = new RequestOptions({headers: this.headers});
+
   }
+
 
   getActivityList():Promise<Activity[]>{
     return new Promise((resolve,reject) => {
-      setTimeout(() => {
-        resolve(this.activities);
-      }, 2000);
+      this.http.get('/api/activity', this.options).subscribe((results) => {
+        console.log(results);
+        resolve(results);
+      });
 
     });
   }
