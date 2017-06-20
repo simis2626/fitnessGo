71907f3940ca688ca496e7983e99303b9c1f5be9
Andromeda <g.simon.levett@gmail.com>
2017-06-20 21:14:35 +1000

    material theme working

--- src/app/activities.service.ts
+++ src/app/activities.service.ts
@@ -1,6 +1,8 @@
+///<reference path="../../node_modules/rxjs/add/operator/map.d.ts"/>
 import {Injectable} from "@angular/core";
 import {Activity} from "./Objects/Activity";
 import {Headers, Http, RequestOptions} from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class ActivitiesService {
@@ -19,10 +21,15 @@ export class ActivitiesService {
 
   }
 
+  private extractData(res) {
+    let body = res.json();
+    return body || {};
+  }
 
   getActivityList():Promise<Activity[]>{
     return new Promise((resolve,reject) => {
-      this.http.get('/api/activity', this.options).subscribe((results) => {
+
+      this.http.get('/api/activity', this.options).map(this.extractData).subscribe((results) => {
         console.log(results);
         resolve(results);
       });
