d9a38b35d29d41944c9d37a853f45574727e4172
Andromeda <g.simon.levett@gmail.com>
2017-06-21 22:26:06 +1000

    target object and user service

---
+++ src/app/target-wo.service.ts
@@ -0,0 +1,54 @@
+import {Injectable} from "@angular/core";
+import {Headers, Http, RequestOptions} from "@angular/http";
+import {Target} from "./Objects/Target";
+
+@Injectable()
+export class TargetWOService {
+
+  headers: Headers;
+  options: RequestOptions;
+
+  constructor(private http: Http) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.options = new RequestOptions({headers: this.headers});
+
+  }
+
+  private extractData(res) {
+    let body = res.json();
+    return body || {};
+  }
+
+
+  changeTarget(target: Target): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+
+      this.http.post('/api/targetwo/', JSON.stringify(target), this.options).map(this.extractData).subscribe((results) => {
+
+        if (results) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+
+      })
+
+
+    });
+
+
+  }
+
+  getTarget(_userId: string): Promise<Target> {
+    return new Promise((resolve, reject) => {
+
+      this.http.get('/api/targetwo/' + _userId).map(this.extractData).subscribe((results) => {
+          resolve(results);
+        }
+      )
+    })
+
+  }
+
+}
