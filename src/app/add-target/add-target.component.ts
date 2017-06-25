210813f1c166e13ea40fccd2c0a1fee1325139aa
andromeda <g.simon.levett@gmail.com>
2017-06-25 18:17:27 +1000

    Cool stuff

---
+++ src/app/add-target/add-target.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {MdSnackBar} from "@angular/material";
+import {TargetWOService} from "../target-wo.service";
+import {Router} from "@angular/router";
+import {Target} from "../Objects/Target";
+
+@Component({
+  selector: 'app-add-target',
+  templateUrl: './add-target.component.html',
+  styleUrls: ['./add-target.component.css', '../material-shared/shared-css.css']
+})
+export class AddTargetComponent implements OnInit {
+
+  constructor(public snackBar: MdSnackBar, public targetService: TargetWOService, private router: Router) {
+  }
+
+  dirty: boolean = false;
+  submitting: boolean = false;
+  public savingText: string;
+  target: Target;
+  private today: Date;
+
+  ngOnInit() {
+    this.today = new Date();
+    this.target = new Target(null,localStorage.getItem('id_token'), this.today);
+
+  }
+
+
+  setDirty() {
+    this.dirty = true;
+  }
+
+
+  onSubmit() {
+    this.savingText = "Saving...";
+    this.submitting = true;
+    this.targetService.changeTarget(this.target).then((result) => {
+        if (result) {
+          this.savingText = "Done!";
+          this.snackBar.open("Saved", null, {duration: 3000});
+          setTimeout(() => {
+            this.router.navigateByUrl('/');
+          }, 800);
+        } else {
+          this.submitting = false;
+          this.snackBar.open("An error occurred. Try again.", null, {duration: 3000});
+        }
+
+
+      }
+    );
+  }
+
+}
