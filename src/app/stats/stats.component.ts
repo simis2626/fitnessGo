ff976af50be6facf0764ccf2d32bb52fea2a6c89
andromeda <g.simon.levett@gmail.com>
2017-07-02 21:10:36 +1000

    flexing

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -4,7 +4,7 @@ import {ActivitiesService} from "../activities.service";
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css']
+  styleUrls: ['./stats.component.css', '../material-shared/shared-css.css']
 })
 export class StatsComponent implements OnInit {
 
@@ -14,14 +14,16 @@ export class StatsComponent implements OnInit {
   weightStats: any;
   activitylist: any[];
   selectedActivity: string;
+  tableStructure: any[];
+  realTableStructure: any[];
 
   ngOnInit() {
     this.activitylist = [];
     this.activityService.getStats(localStorage.getItem('id_sub')).then((weightStats) => {
 
       this.weightStats = weightStats[0];
-      for (let i = 0; i < this.weightStats.personalBests.length; i++) {
-        this.activitylist.push(this.weightStats.personalBests[i]._id.name);
+      for (let i = 0; i < this.weightStats.activityNames.length; i++) {
+        this.activitylist.push(this.weightStats.activityNames[i]._id.name);
       }
     });
 
@@ -29,7 +31,61 @@ export class StatsComponent implements OnInit {
 
 
   populateTable() {
-    console.log("trigger");
+    let compFunction = function (a, b) {
+      if (a._id.weight > b._id.weight) {
+        return 1;
+      } else if (a._id.weight < b._id.weight) {
+        return -1;
+      }
+      return 0;
+    };
+
+
+    console.log(this.selectedActivity);
+    this.tableStructure = [[], []];
+    let filteredPBArray = this.weightStats.personalBests.filter(function (act) {
+      return act._id.name == this.selectedActivity;
+    }, this);
+    let filteredRecentArray = this.weightStats.mostRecent.filter(function (act) {
+      return act._id.name == this.selectedActivity;
+    }, this);
+
+    filteredPBArray.sort(compFunction);
+    filteredRecentArray.sort(compFunction);
+    let value = 0;
+    for (let weight of filteredPBArray) {
+      this.tableStructure[value][0] = weight._id.weight + 'kg';
+      this.tableStructure[value][1] = weight.PbReps;
+      value++;
+    }
+    value = 0;
+    for (let weight of filteredRecentArray) {
+      this.tableStructure[value][2] = weight.reps;
+      value++;
+    }
+    this.realTableStructure = this.tableStructure;
   }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
