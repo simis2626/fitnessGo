3bd4cffacd8b373477790e64930c801bab59f260
Andromeda <g.simon.levett@gmail.com>
2017-09-04 15:21:08 +1000

    storing fitbit code

--- src/app/workouts.service.ts
+++ src/app/workouts.service.ts
@@ -17,6 +17,7 @@ export class WorkoutsService {
   constructor(private http: Http) {
     this.headers = new Headers();
     this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Authorization', 'Bearer ' + localStorage.getItem('access_token'));
     this.options = new RequestOptions({headers: this.headers});
 
   }
